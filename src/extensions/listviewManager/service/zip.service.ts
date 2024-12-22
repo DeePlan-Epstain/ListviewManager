2fc10f00bc1913df54b0ceb382d2b63e9f7c826c
benE-deeplan <150326766+benE-deeplan@users.noreply.github.com>
2024-12-22 14:26:17 +0200

    Initial commit

--- src/extensions/listviewManager/service/zip.service.ts
+++ src/extensions/listviewManager/service/zip.service.ts
@@ -1,14 +1,10 @@
 import { SelectedFile, File } from '../models/global.model';
 import JSZip, { file } from 'jszip';
 import { ListViewCommandSetContext } from "@microsoft/sp-listview-extensibility";
-import { SPHttpClient, SPHttpClientResponse } from '@microsoft/sp-http';
-import getSP from '../../../pnpjs-config';
+import { getSP } from '../../../pnpjs-config';
 import { SPFI } from '@pnp/sp';
-import "@pnp/sp/folders";
 import { folderFromServerRelativePath } from "@pnp/sp/folders";
 import { IFileInfo } from '@pnp/sp/files';
-import "@pnp/sp/webs";
-import "@pnp/sp/batching";
 
 
 const DELAY_TIME = 5000;
@@ -22,29 +18,26 @@ function delay(ms: number): Promise<void> {
 }
 
 // a function that count the number of http calls made and delay if needed to prevent throttle
-async function delayIfNeeded(){
+async function delayIfNeeded() {
     HTTP_COUNTER++;
-    console.log(`http call counter: ${HTTP_COUNTER}`);
-    if(HTTP_COUNTER >= MAX_HTTP_CALLS){
+    if (HTTP_COUNTER >= MAX_HTTP_CALLS) {
         HTTP_COUNTER = 0;
         await delay(DELAY_TIME);
     }
 }
 
 // main function that trigger the recursive calls
-export async function exportToZip(selectedItems: SelectedFile[], context: ListViewCommandSetContext ){
+export async function exportToZip(selectedItems: SelectedFile[], context: ListViewCommandSetContext) {
     console.time("exportToZip")
-    console.log("context: ", context);
     const archive = new JSZip();
-    console.log(selectedItems);
     const rootFolderName = getRootFolder(selectedItems[0]);
     const sp = getSP(context);
     //handle the root folder
-    for(const item of selectedItems){
-        if(item.FSObjType === "1"){// folder
+    for (const item of selectedItems) {
+        if (item.FSObjType === "1") {// folder
             await handleFolder(rootFolderName, archive, item, sp);
         }
-        else{// file
+        else {// file
             await handleFile(rootFolderName, archive, item, sp);
         }
     }
@@ -53,9 +46,8 @@ export async function exportToZip(selectedItems: SelectedFile[], context: ListVi
 }
 
 // return the root folder of the zip
-function getRootFolder(selectedFile: File){
+function getRootFolder(selectedFile: File) {
     const root = selectedFile.FileRef.split('/');
-    console.log("root: ", root[root.length - 2]);
     return root[root.length - 2];
 }
 
@@ -69,35 +61,35 @@ function getFolderPath(selectedFile: SelectedFile | File, rootFolderName: string
 }
 
 // This function get a selected item and add its content to the new zip archive
-async function handleFile(rootFolder: string, archive: JSZip, selectedFile: File, sp: SPFI){
-    try{
+async function handleFile(rootFolder: string, archive: JSZip, selectedFile: File, sp: SPFI) {
+    try {
         const fileContent = await sp.web.getFileByServerRelativePath(selectedFile.FileRef).getBuffer();
         delayIfNeeded();
         const filePath = getFolderPath(selectedFile, rootFolder);
-        if(filePath){
-            archive.file(filePath, fileContent);            
+        if (filePath) {
+            archive.file(filePath, fileContent);
         }
-    } catch(error){
+    } catch (error) {
         console.error(`Error downloading file: ${selectedFile.FileRef} error: `, error)
     }
 }
 
 // This function get a selected item and add its content to the new zip archive
-function handleFileBatched(rootFolder: string, archive: JSZip, selectedFile: File, sp: SPFI, fileContent: ArrayBuffer){
-    try{
+function handleFileBatched(rootFolder: string, archive: JSZip, selectedFile: File, sp: SPFI, fileContent: ArrayBuffer) {
+    try {
         // const fileContent = await sp.web.getFileByServerRelativePath(selectedFile.FileRef).getBuffer();
         // delayIfNeeded();
         const filePath = getFolderPath(selectedFile, rootFolder);
-        if(filePath){
-            archive.file(filePath, fileContent);            
+        if (filePath) {
+            archive.file(filePath, fileContent);
         }
-    } catch(error){
+    } catch (error) {
         console.error(`Error downloading file: ${selectedFile.FileRef} error: `, error)
     }
 }
 
-async function handleFolder(rootFolder: string, archive: JSZip, selectedFile: File, sp: SPFI){
-    try{
+async function handleFolder(rootFolder: string, archive: JSZip, selectedFile: File, sp: SPFI) {
+    try {
         archive.folder(getFolderPath(selectedFile, rootFolder));
         const selectedFolder = folderFromServerRelativePath(sp.web, selectedFile.FileRef);
         // const folderInfo = await selectedFolder();
@@ -107,30 +99,30 @@ async function handleFolder(rootFolder: string, archive: JSZip, selectedFile: Fi
         let [batchedWeb, execute] = sp.web.batched();
         let res: ArrayBuffer[] = [];
         //handle files within the folder
-        for(const file of files){
+        for (const file of files) {
             batchedWeb.getFileByServerRelativePath(file.ServerRelativeUrl).getBuffer().then(content => res.push(content));
             batchcounter++;
-            if(batchcounter % 100 === 0){
+            if (batchcounter % 100 === 0) {
                 await execute();
                 batchcounter = 0;
                 [batchedWeb, execute] = sp.web.batched();
             }
         }
-        files.forEach(async (file, index) =>{
-            handleFileBatched(rootFolder, archive, { FileRef: files[index].ServerRelativeUrl, FileLeafRef: files[index].Name}, sp, res[index])
+        files.forEach(async (file, index) => {
+            handleFileBatched(rootFolder, archive, { FileRef: files[index].ServerRelativeUrl, FileLeafRef: files[index].Name }, sp, res[index])
         })
         //handle sub folders
-        for(const subfolder of folders){
-            await handleFolder(rootFolder, archive, { FileRef: subfolder.ServerRelativeUrl, FileLeafRef: subfolder.Name}, sp)
+        for (const subfolder of folders) {
+            await handleFolder(rootFolder, archive, { FileRef: subfolder.ServerRelativeUrl, FileLeafRef: subfolder.Name }, sp)
         }
     }
-    catch(error){
+    catch (error) {
         console.error(error);
     }
 }
 
 // download the zip archive to my computer
-export async function downloadToPC(archive:JSZip) {
+export async function downloadToPC(archive: JSZip) {
     archive.generateAsync({ type: "blob" }).then((content) => {
         const link = document.createElement("a");
         link.href = URL.createObjectURL(content);
@@ -146,10 +138,8 @@ export async function saveZipToSharePoint(archive: JSZip, selectedItems: Selecte
         const zipBlob = await archive.generateAsync({ type: "blob" });
         const folderPath = selectedItems[0].FileRef.substring(0, selectedItems[0].FileRef.lastIndexOf('/'));
         const zipFileName = `${rootFolderName}_ExportedFiles.zip`;
-        console.log("begin uploading the zip file");       
         // Upload the zip file to SharePoint
         await sp.web.getFolderByServerRelativePath(folderPath).files.addChunked(zipFileName, zipBlob);
-        console.log(`Zip file successfully uploaded to SharePoint folder: ${folderPath}`);
     } catch (error) {
         console.error("Error saving zip file to SharePoint:", error);
     }
